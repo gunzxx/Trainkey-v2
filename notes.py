10f6adf58d3c331cd0b8c82cb71e871c096b5ac6
Guntur W <ggm5850@gmail.com>
2024-11-28 01:18:38 +0700

    feat: notes

---
+++ notes.py
@@ -0,0 +1,16 @@
+###
+# Trainkey list:
+# - jwt
+# - setup jwt
+# - media library
+# - update register & others
+# - authed
+# - shuffle
+# - api kata
+# - update profil
+# - menu layer
+# - forum
+# - lupa password
+# - help & support
+# - documentation
+###
